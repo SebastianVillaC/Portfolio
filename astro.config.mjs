c07494b0f6ca2336392e5044c4e2ee33e4296ae3
Sebastian Villa <sebasvilla106@gmail.com>
2026-01-15 17:04:51 -0500

    yaml for publish site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site:'https://github.com/SebastianVillaC',
+  base:'/Portfolio/'
 });
